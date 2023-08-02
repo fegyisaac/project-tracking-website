2eae65cee8497dfb9920659d4e14be127fd465e0
zicojr <fegyisaac160@gmail.com>
2023-08-02 17:02:06 +0100

    finished mobile view

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,20 @@
-const hamburger = document.getElementById("hamburger")
-const close = document.getElementById("close")
 
-hamburger.addEventListener("onclick", {
-    
+const navls = document.getElementById('nav_list');
+const hamburger = document.getElementById('hamburger');
+const close = document.getElementById('close')
+
+
+hamburger.addEventListener('click', (e) => {
+    e.preventDefault();
+    hamburger.style.display = 'none';
+    close.style.display = 'block';
+    navls.style.display = 'block';
+})
+
+close.addEventListener('click', (e) => {
+    e.preventDefault();
+    close.style.display = 'none';
+    hamburger.style.display = 'block';
+    navls.style.display = 'none';
+
 })
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,2 @@
-const hamburger=document.getElementById("hamburger"),close=document.getElementById("close");hamburger.addEventListener("onclick",{});
+const navls=document.getElementById("nav_list"),hamburger=document.getElementById("hamburger"),close=document.getElementById("close");hamburger.addEventListener("click",(e=>{e.preventDefault(),hamburger.style.display="none",close.style.display="block",navls.style.display="block"})),close.addEventListener("click",(e=>{e.preventDefault(),close.style.display="none",hamburger.style.display="block",navls.style.display="none"}));
 //# sourceMappingURL=script.js.map
