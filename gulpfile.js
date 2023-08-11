1372da937c3401333ce3757674781a350ec2e7ce
zicojr <fegyisaac160@gmail.com>
2023-08-11 16:57:23 +0100

    done

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const terser = require('gulp-terser');
-const browsersync = require('browser-sync').create();
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
 
 // Sass Task
 function scssTask() {
-  return src('app/scss/styles.scss', { sourcemaps: true })
+  return src("app/scss/styles.scss", { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([cssnano()]))
-    .pipe(dest('dist/css', { sourcemaps: '.' }))
+    .pipe(dest("dist/css", { sourcemaps: "." }));
 }
 
 // JS Task
 function jsTask() {
-  return src('app/js/script.js', { sourcemaps: true })
+  return src("app/js/script.js", { sourcemaps: true })
     .pipe(terser())
-    .pipe(dest('dist/js', { sourcemaps: '.' }))
+    .pipe(dest("dist/js", { sourcemaps: "." }));
 }
 
 // Browsersync Func
 function browsersyncServe(cb) {
   browsersync.init({
     server: {
-      baseDir: '.'
-    }
+      baseDir: ".",
+    },
   });
   cb();
 }
@@ -38,15 +38,12 @@ function browsersyncReload(cb) {
 
 // Watch Task
 function watchTask() {
-  watch('*.html', browsersyncReload)
-  watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload))
-
+  watch("*.html", browsersyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
+    series(scssTask, jsTask, browsersyncReload)
+  );
 }
 
 // Default Gulp tast
-exports.default = series(
-  scssTask,
-  jsTask,
-  browsersyncServe,
-  watchTask,
-)
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
